582e046c9d6cf967a165375e1fc5e78418ab4e0e
raizakurniawan <raizakurniawan@gmail.com>
2024-12-18 18:02:43 +0700

    Fixing scrollIntoView

--- cypress/e2e/mythicRepo/quickLinks.spec.js
+++ cypress/e2e/mythicRepo/quickLinks.spec.js
@@ -2,28 +2,71 @@
 
 describe('Quick Links', () => {
     beforeEach(() => {
-        cy.visit('/')
+        cy
+            .visit('/')
+            .get('.QuickLink_quick-link_banner__sw2wM')
+            .click()
     });
 
     it('Verify that the user able to open the Protocol Handbook page', () => {
-        cy
-            .get('.QuickLink_quick-link_banner__sw2wM')
-            .click()
-
         cy 
             .get('.Handbook_container__TUXbq')
-            .contains('Protocol Handbook')
+            .contains('PROTOCOL HANDBOOK')
     });
 
     it('Verify that the user able to see the Introductory Letter', () => {
-        cy
-            .get('.QuickLink_quick-link_banner__sw2wM')
-            .click()
-
         cy 
             .get('.Handbook_container__TUXbq')
             .contains('Introductory Letter')
-            .contains('Welcome Operative')
-            
     });
+
+    it('Verify that the user able to see the What is the Mythic Protocol', () => {
+        cy 
+            .get('.Handbook_text-wrapper__GqT01')
+            .contains('What is Mythic Protocol')
+    });
+
+    it('Verify that the user able to see the Emporium in faction background section', () => {
+        cy
+            .wait(2000)
+            .get('.Handbook_text-wrapper__GqT01')
+            .scrollIntoView().should('be.visible')
+            .wait(1000)
+            .get('.Handbook_component-expand__Pgk04')
+            .scrollIntoView().should('be.visible')
+            .contains('EMPORIUM').click()
+    });
+
+    it('Verify that the user able to see the Naga Society in faction background section', () => {
+        cy 
+            .get('.Handbook_text-wrapper__GqT01')
+            .scrollIntoView().should('be.visible')
+            .wait(1000)
+            .get('.Handbook_component-expand__Pgk04')
+            .scrollIntoView().should('be.visible')
+            .contains('NAGA SOCIETY').click()
+    });
+
+    it('Verify that the user able to see the Maledictum in faction background section', () => {
+        cy 
+            .get('.Handbook_text-wrapper__GqT01')
+            .scrollIntoView().should('be.visible')
+            .wait(1000)
+            .get('.Handbook_component-expand__Pgk04')
+            .scrollIntoView().should('be.visible')
+            .contains('MALEDICTUM').click()
+    });
+
+
+    it('Verify that the user able to see the Interregnum in faction background section', () => {
+        cy 
+            .get('.Handbook_text-wrapper__GqT01')
+            .scrollIntoView().should('be.visible')
+            .wait(1000)
+            .get('.Handbook_component-expand__Pgk04')
+            .scrollIntoView().should('be.visible')
+            .contains('INTERREGNUM').click()
+    });
+
+    
 });
